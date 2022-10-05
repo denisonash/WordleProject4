93a93d2695d92aa7342aa50bc2d905dddc936704
Theasus7 <anderson.taylor98@gmail.com>
2022-10-05 11:49:36 -0600

    Test

--- Wordle.py
+++ Wordle.py
@@ -6,7 +6,7 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 """
 
 import random
-
+#test
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
